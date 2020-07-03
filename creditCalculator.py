b391b388fe29d09b799269dc4898e59efe756088
Tim Aadland <potetstapp@gmail.com>
2020-07-03 09:05:46 +0200

    Added description and some variables in creditCalculator.py

---
+++ creditCalculator.py
@@ -0,0 +1,17 @@
+'''
+This program takes in several parameters like a credit interest, credit principal and calculates
+values like monthly payment or overpayment and print it out on the monitor
+credit principal is the original money on the credit, while the interest is a charge for borrowed money.
+'''
+
+credit_principal = 'Credit principal: 1000'
+final_output = 'The credit has been repaid!'
+first_month = 'Month 1: paid out 250'
+second_month = 'Month 2: paid out 250'
+third_month = 'Month 3: paid out 500'
+
+print(credit_principal)
+print(first_month)
+print(second_month)
+print(third_month)
+print(final_output)
