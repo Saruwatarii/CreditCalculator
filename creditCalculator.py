59affb5a7eb5ffe806df46b422670f8e4467f11d
Tim Aadland <potetstapp@gmail.com>
2020-07-06 08:06:15 +0200

    Added input for credit principal, option for monthly (m) or counts of periods (p). In order to make the payments easier without floats, the payments gets rounded up, and the last payment calculate the correctly according to the rounding up months

--- creditCalculator.py
+++ creditCalculator.py
@@ -1,17 +1,30 @@
 '''
-This program takes in several parameters like a credit interest, credit principal and calculates
-values like monthly payment or overpayment and print it out on the monitor
+This program takes in two parameters like a credit interest and credit principal, and calculates
+values like monthly payment or overpayment which then is printed out on the monitor
 credit principal is the original money on the credit, while the interest is a charge for borrowed money.
 '''
 
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
+import math
+credit_principal = int(input("Enter the credit principal: "))
 
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+months_or_monthly_payment = input(
+"""What do you want to calculate?
+type "m" - for count of months,
+type "p" - for monthly payments: """)
+
+if months_or_monthly_payment == "m":
+    monthly = int(input("Enter monthly payment: "))
+    payment = math.ceil(credit_principal / monthly)
+    if payment >= 2:
+        print(f"It takes {payment} months to repay the credit")
+    else:
+        print(f"It takes {payment} month to repay the credit")
+
+if months_or_monthly_payment == "p":
+    counts = int(input("Enter count of months: "))
+    each_month = math.ceil(credit_principal / counts)
+    last_payment = credit_principal - (counts - 1) * each_month
+    if each_month != last_payment:
+        print(f"Your monthly payment = {each_month} with last month payment {last_payment}")
+    else:
+        print(f"Your monthly payment = {each_month}")
